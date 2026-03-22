37cdfe1b6967111c40070c937aa6e8963ef0f37b
Sebastien Ramsay <sebastien.ramsay@gmail.com>
2026-03-22 06:38:03 -0400

    Slider: added min, max, decimals, removed noclippling on grip, added isSameValue in OnValueChanged(), added GetValue()

--- lua/pixelui/elements/cl_slider.lua
+++ lua/pixelui/elements/cl_slider.lua
@@ -17,16 +17,25 @@
 
 local PANEL = {}
 
+AccessorFunc(PANEL, "Min", "Min", FORCE_NUMBER)
+AccessorFunc(PANEL, "Max", "Max", FORCE_NUMBER)
+AccessorFunc(PANEL, "Decimals", "Decimals", FORCE_NUMBER)
+
 function PANEL:Init()
     self.Fraction = 0
+    self:SetDecimals(0) -- default = integers
+    self:SetMin(0)
+    self:SetMax(1)
 
     self.Grip = vgui.Create("PIXEL.ImageButton", self)
-    self.Grip:NoClipping(true)
+    self.Grip:NoClipping(false)
 
     self.Grip:SetImageURL("https://pixel-cdn.lythium.dev/i/g6e8z4pz")
     self.Grip:SetNormalColor(PIXEL.CopyColor(PIXEL.Colors.Primary))
     self.Grip:SetHoverColor(PIXEL.OffsetColor(PIXEL.Colors.Primary, -15))
     self.Grip:SetClickColor(PIXEL.OffsetColor(PIXEL.Colors.Primary, 15))
+
+    self.LastValue = self:GetValue()
 
     self.Grip.OnCursorMoved = function(pnl, x, y)
         if not pnl.Depressed then return end
@@ -36,8 +45,9 @@ function PANEL:Init()
 
         self.Fraction = math.Clamp(x / self:GetWide(), 0, 1)
 
-        self:OnValueChanged(self.Fraction)
+        self:OnValueChanged(self:GetValue(), self.LastValue == self:GetValue())
         self:InvalidateLayout()
+        self.LastValue = self:GetValue()
     end
 
     self.BackgroundCol = PIXEL.OffsetColor(PIXEL.Colors.Background, 20)
@@ -48,23 +58,49 @@ function PANEL:OnMousePressed()
     local w = self:GetWide()
 
     self.Fraction = math.Clamp(self:CursorPos() / w, 0, 1)
-    self:OnValueChanged(self.Fraction)
+    self:OnValueChanged(self:GetValue(), self.LastValue == self:GetValue())
     self:InvalidateLayout()
 end
 
-function PANEL:OnValueChanged(fraction) end
+function PANEL:GetValue()
+    local val = Lerp(self.Fraction, self:GetMin(), self:GetMax())
+
+    local decimals = self:GetDecimals() or 0
+    local mult = 10 ^ decimals
+
+    return math.Round(val * mult) / mult
+end
+
+function PANEL:SetValue(val)
+    local min, max = self:GetMin(), self:GetMax()
+    self.Fraction = math.Clamp((val - min) / (max - min), 0, 1)
+
+    self:OnValueChanged(self:GetValue(), self.LastValue == self:GetValue())
+    self:InvalidateLayout()
+end
+
+function PANEL:OnValueChanged(fraction, isSameValue) end
 
 function PANEL:Paint(w, h)
     local rounding = h * .5
-    PIXEL.DrawRoundedBox(rounding, 0, 0, w, h, self.BackgroundCol)
-    PIXEL.DrawRoundedBox(rounding, 0, 0, self.Fraction * w, h, self.FillCol)
+    local pad = h * 0.5
+    local usableW = w - (pad * 2)
+
+    local fillW = self.Fraction * usableW
+
+    PIXEL.DrawRoundedBox(rounding, pad, h / 4, usableW, h / 2, self.BackgroundCol)
+    PIXEL.DrawRoundedBox(rounding, pad, h / 4, fillW, h / 2, self.FillCol)
 end
 
 function PANEL:PerformLayout(w, h)
-    local gripSize = h + PIXEL.Scale(6)
-    local offset = PIXEL.Scale(3)
-    self.Grip:SetSize(gripSize, gripSize)
-    self.Grip:SetPos((self.Fraction * w) - (gripSize * .5), -offset)
+    local gripW = h
+    self.Grip:SetSize(h, h)
+
+    local pad = gripW * 0.5
+    local usableW = w - (pad * 2)
+    local x = pad + (self.Fraction * usableW)
+
+    self.Grip:SetPos(x - (gripW * 0.5), 0)
 end
 
 vgui.Register("PIXEL.Slider", PANEL, "PIXEL.Button")
